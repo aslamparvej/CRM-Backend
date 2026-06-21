3515944b4762562846aa3b7fbc8b157c8865ff6d
Aslam Parvej <aslamparvej2@gmail.com>
2026-06-21 11:04:25 +0530

    Added lead filter helper

---
+++ helpers/filter.js
@@ -0,0 +1,32 @@
+import User from "../models/User.js";
+
+export const buildLeadFilter = async (user) => {
+  const match = { isDeleted: false };
+
+  if (user?.role === "admin") {
+    return match;
+  }
+
+  if (user?.role === "executive") {
+    match.$or = [{ createdBy: user.id }, { assignedTo: user.id }];
+  }
+
+  if (user?.role === "sub-admin") {
+    const executives = await User.find({
+      role: "executive",
+      createdBy: user.id,
+    }).select("_id");
+
+    const executiveIds = executives.map((e) => e._id);
+
+    match.$or = [
+        {createdBy: user.id},
+        {createdBy: {$in: executiveIds}},
+        {assignedTo: {$in: executiveIds}},
+    ];
+
+    return match;
+  }
+
+  return match;
+};
